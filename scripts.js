29b9230bca9219108f829be29dbd52e46648a371
jppastorek <jppastorek@gmail.com>
2022-01-08 16:30:38 -0600

    began to add score to 5.

--- scripts.js
+++ scripts.js
@@ -4,29 +4,6 @@ let computerPlay = function() {
     return computerSelection;
 }
 
-function playRock() {
-    playerImage.src = "./images/rock.png";
-    let compChoice = computerPlay();
-    computerImage.src = `./images/${compChoice}.png`;
-    // let playerSelection = "rock";
-    // return playerSelection;
-}
-
-function playPaper() {
-    playerImage.src = "./images/paper.png";
-    let compChoice = computerPlay();
-    computerImage.src = `./images/${compChoice}.png`;
-    // let playerSelection = "paper";
-    // return playerSelection;
-}
-
-function playScissors() {
-    playerImage.src = "./images/scissors.png";
-    let compChoice = computerPlay();
-    computerImage.src = `./images/${compChoice}.png`;
-    // let playerSelection = "scissors";
-    // return playerSelection;
-}
 
 function setPlayerWinnerClass() {
     if (!playerHand.classList.contains("player-winner")) {
@@ -56,36 +33,45 @@ function setTieClass() {
 }
 
 
-function playRound(playerSelection, computerSelection) {
-    let winner;
-
-    if (playerSelection === "rock" && computerSelection === "scissors") {
-        winner = "Player";
-        resultText.innerText = 'You win! Play again?';
-        setPlayerWinnerClass();
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        winner = "Player";
-        resultText.innerText = 'You win! Play again?';
-        setPlayerWinnerClass();
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        winner = "Player";
-        resultText.innerText = 'You win! Play again?';
-        setPlayerWinnerClass();
-    } else if (playerSelection === "rock" && computerSelection === "paper") {
-        winner = "Computer";
-        resultText.innerText = 'You lose! Play again?';
-        setComputerWinnerClass();
-    } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        winner = "Computer";
-        resultText.innerText = 'You lose! Play again?';
-        setComputerWinnerClass();
-    } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        winner = "Computer";
-        resultText.innerText = 'You lose! Play again?';
-        setComputerWinnerClass();
+function addPlayerScore() {
+    if (playerScore < 3) {
+        playerScore++;
+        resultText.innerText = `Player: ${playerScore} --- Computer: ${computerScore}`;
     } else {
-        winner = "No one";
-        resultText.innerText = "It's a tie! Play again?";
+        resultText.innerText = `You win! The score was ${playerScore} to ${computerScore}.`
+    }
+}
+
+function addComputerScore() {
+    if (computerScore < 3) {
+        computerScore++;
+        resultText.innerText = `Player: ${playerScore} --- Computer: ${computerScore}`;
+    } else {
+        resultText.innerText = `You lose! The score was ${playerScore} to ${computerScore}.`
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock" && computerSelection === "scissors") {
+        setPlayerWinnerClass();
+        addPlayerScore();
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        setPlayerWinnerClass();
+        addPlayerScore();
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        setPlayerWinnerClass();
+        addPlayerScore();
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        setComputerWinnerClass();
+        addComputerScore();
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        setComputerWinnerClass();
+        addComputerScore();
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        setComputerWinnerClass();
+        addComputerScore();
+    } else {
+        resultText.innerText = `It's a tie! Player: ${playerScore} --- Computer: ${computerScore}`;
         setTieClass();
     }
 
@@ -101,8 +87,8 @@ const computerImage = document.querySelector(".computer-img");
 const resultText = document.querySelector(".result-text");
 const playerHand = document.querySelector(".player-hand");
 const computerHand = document.querySelector(".computer-hand");
-
-
+let playerScore = 0;
+let computerScore = 0;
 
 rockSelector.addEventListener("click", function(e) {
     let playerSelection = "rock";
@@ -155,88 +141,3 @@ function game() {
     }
     return victor;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// THIS IS THE OLD WAY, WITHOUT RETURNS IN THE FUNCTIONS
-
-// let playerScore = 0
-// let compScore = 0
-// let playerChoice = ""
-// let compChoice = ""
-// let choices = ["rock", "paper", "scissors"]
-// let winnerOfRound = ""
-
-// function setChoices() {
-//     playerChoice = prompt("Choose your weapon...")
-//     playerChoice = playerChoice.toLowerCase()
-//     compChoice = choices[Math.floor(Math.random() * 3)]
-//     console.log(`We have Player 1: ${playerChoice} vs Computer: ${compChoice}`)
-// }
-
-// function logWinner() {
-//     console.log(`The ${winnerOfRound} wins. The score is ${playerScore} to ${compScore}.`)
-// }
-
-
-// function showResult() {
-//     if (winnerOfRound != "player" && winnerOfRound != "computer") {
-//         console.log(`It's a tie. The score is ${playerScore} to ${compScore}.`)
-//     } else {
-//         logWinner();
-//     }
-// }
-
-
-// //determine winnerOfRound with simple if-then
-
-// function determineWinner() {
-//     setChoices();
-//     if (playerChoice === "rock" && compChoice === "scissors") {
-//         winnerOfRound = "player";
-//         playerScore++;
-//     } else if (playerChoice === "rock" && compChoice === "paper") {
-//         winnerOfRound = "computer";
-//         compScore++;
-//     } else if (playerChoice === "paper" && compChoice === "scissors") {
-//         winnerOfRound = "computer";
-//         compScore++;
-//     } else if (playerChoice === "paper" && compChoice === "rock") {
-//         winnerOfRound = "player";
-//         playerScore++;
-//     } else if (playerChoice === "scissors" && compChoice === "rock") {
-//         winnerOfRound = "computer";
-//         compScore++;
-//     } else if (playerChoice === "scissors" && compChoice === "paper") {
-//         winnerOfRound = "player";
-//         playerScore++;
-//     } else {
-//         winnerOfRound = "No one";
-//     }
-//     showResult();
-// }
-
-// //game while loop to determine a winner best out of 5
-// function game() {
-//     while (playerScore < 3 && compScore < 3) {
-//         determineWinner();
-//         if (playerScore === 3) {
-//             console.log(`The player wins whole game: ${playerScore} to ${compScore}.`);
-//         } else if (compScore === 3) {
-//             console.log(`The computer wins whole game: ${playerScore} to ${compScore}.`);
-//         }
-//     }
-// }
-
-// game();
